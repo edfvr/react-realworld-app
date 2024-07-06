8f910e739ae8bf6266f17fcf581449665b9566cc
Ed <edsagoefvr@gmail.com>
2024-07-06 20:42:20 +0000

    Install redux

---
+++ src/redux/authSlice.ts
@@ -0,0 +1,28 @@
+// authSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+    token: null,
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUser, setToken, logout } = authSlice.actions;
+export default authSlice.reducer;
