8956e56dfc76a88e8e328a0d842dba3abf0c5221
Ed <edsagoefvr@gmail.com>
2024-07-07 16:29:00 +0000

    Edited a comment

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,9 +4,6 @@ interface PaginationProps {
     onPageChange: (page: number) => void; // Callback function to handle page changes
   }
   
-  /**
-   * Pagination component
-   */
   export default function Pagination({ currentPage, totalPages, onPageChange }: PaginationProps): JSX.Element {
     
     // Make an array of page numbers based on totalPages
